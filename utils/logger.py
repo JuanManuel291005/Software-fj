6dd0e44eff12a8bfc57fbee85233fc6dcaab331f
JuanManuel291005 <Juan.manuel291005@gmail.com>
2026-05-05 16:38:21 -0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,11 @@
+import logging
 
+# Configuración del archivo de logs
+logging.basicConfig(
+    filename="errores.log",
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def registrar_error(error):
+    logging.error(str(error))
