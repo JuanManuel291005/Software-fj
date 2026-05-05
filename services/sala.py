c6cd51337c59f5ef2406866ad44992472129ef40
JuanManuel291005 <Juan.manuel291005@gmail.com>
2026-05-05 16:40:33 -0500

    Update sala.py

--- services/sala.py
+++ services/sala.py
@@ -1,1 +1,16 @@
+from modelos.servicio import Servicio
+from utilidades.excepciones import ErrorServicio
 
+
+class ServicioSala(Servicio):
+
+    def calcular_costo(self, horas):
+        try:
+            if horas <= 0:
+                raise ErrorServicio("Horas inválidas")
+            return horas * 50
+        except Exception as e:
+            raise ErrorServicio("Error en servicio de sala") from e
+
+    def descripcion(self):
+        return "Reserva de sala por horas"
