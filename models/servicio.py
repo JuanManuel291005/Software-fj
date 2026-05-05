4b8bacd6b4b69aad4c324536e034772f01809d4b
JuanManuel291005 <Juan.manuel291005@gmail.com>
2026-05-05 16:39:56 -0500

    Update servicio.py

--- models/servicio.py
+++ models/servicio.py
@@ -1,1 +1,24 @@
+from abc import ABC, abstractmethod
+from utilidades.excepciones import ErrorServicio
 
+
+class Servicio(ABC):
+
+    @abstractmethod
+    def calcular_costo(self, duracion):
+        """Debe ser implementado por cada servicio"""
+        pass
+
+    @abstractmethod
+    def descripcion(self):
+        """Describe el servicio"""
+        pass
+
+    # Método con "sobrecarga simulada"
+    def calcular_costo_con_extra(self, duracion, impuesto=0, descuento=0):
+        try:
+            base = self.calcular_costo(duracion)
+            total = base + (base * impuesto) - descuento
+            return total
+        except Exception as e:
+            raise ErrorServicio("Error al calcular costo") from e
