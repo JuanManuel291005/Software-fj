15fb2462c8d7e0b63b67356a6242ea2eecddbb58
JuanManuel291005 <Juan.manuel291005@gmail.com>
2026-05-05 16:37:13 -0500

    Update excepciones.py

--- utils/excepciones.py
+++ utils/excepciones.py
@@ -1,1 +1,18 @@
+class ErrorSistema(Exception):
+    """Clase base para errores del sistema"""
+    pass
 
+
+class ErrorCliente(ErrorSistema):
+    """Errores relacionados con clientes"""
+    pass
+
+
+class ErrorServicio(ErrorSistema):
+    """Errores relacionados con servicios"""
+    pass
+
+
+class ErrorReserva(ErrorSistema):
+    """Errores relacionados con reservas"""
+    pass
